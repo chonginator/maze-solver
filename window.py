0d4cf8f8bc3ef5bef78bc512f460e13be66137c4
chonginator <benchong1999@yahoo.com.au>
2024-07-03 20:40:13 +1000

    add Window class

---
+++ window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+  def __init__(self, width, height):
+    self.__root = Tk()
+    self.__root.title = "Maze Solver"
+    self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    self.__canvas = Canvas(width=width, height=height)
+    self.__canvas.pack()
+
+    self.__running = False
+
+  def redraw(self):
+    self.__root.update_idletasks()
+    self.__root.update()
+  
+  def wait_for_close(self):
+    self.__running = True
+
+    while self.__running:
+      self.redraw()
+
+  def close(self):
+    print("close window!")
+    self.__running = False
