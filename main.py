4b2209db5a95adab3c69459a85399e2f8d4bf658
chonginator <benchong1999@yahoo.com.au>
2024-07-04 12:49:11 +1000

    add line drawing functionality

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from window import Window
+from graphics import Window, Line, Point
 
 if __name__ == "__main__":
   win = Window(800, 600)
+  line_a = Line(
+    Point(250, 250),
+    Point(500, 500)
+  )
+  line_b = Line(
+    Point(350, 150),
+    Point(720, 400)
+  )
+  win.draw_line(line_a, "black")
+  win.draw_line(line_b, "red")
   win.wait_for_close()
